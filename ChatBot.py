43b099fa9010ce9cd77335006e83c34794296e7c
larios <arallariosqu@ittepic.edu.mx>
2021-01-12 22:58:38 -0600

    integracion con telegram

--- ChatBot.py
+++ ChatBot.py
@@ -1,4 +1,5 @@
 from ChatBotBrain import ChatBotBrain
+from Api import Api
 import re
 
 class ChatBot:
@@ -6,20 +7,28 @@ class ChatBot:
         self.brain = ChatBotBrain(context, seed, model, tokenizer)
         self.first_message = first_message
         self.init_answers()
+        self.api = Api()
 
     def run(self):
-        print('RoboTec dice >>', self.first_message)
         while True:
-            question = input('>> ')
-            if self.filter_question(question):
-                print('RoboTec dice >>', self.brain.talk(question))
+            dictionary = self.api.update(self.api.last_message)
+            for m in dictionary['result']:
+                id_chat, user, question, id_update = self.api.read_messages(m)
 
-    def filter_question(self, question):
+                if id_update > (self.api.last_message-1):
+                    self.api.last_message = id_update + 1
+
+                if self.filter_question(question.lower(), id_chat):
+                    self.api.send_message(id_chat, self.brain.talk(question))
+                    # print('RoboTec dice >>', self.brain.talk(question))
+
+    def filter_question(self, question, id_chat):
         for answer in self.answers:
             pattern = answer[0]
             p = re.compile(pattern)
-            if not p.search(question) is None:
-                print('RoboTec dice >>', answer[1])
+            if not p.search(question.lower()) is None:
+                self.api.send_message(id_chat, answer[1])
+                # print('RoboTec dice >>', answer[1])
                 return False
         return True
         
@@ -34,4 +43,8 @@ class ChatBot:
                 '(.*)gg(.*)f(.*)',
                 'parece que jala xd'
             ),
+            (
+                'hola',
+                self.first_message
+            )
         ]
--- Main.py
+++ Main.py
@@ -1,9 +1,7 @@
 from ChatBot import ChatBot
 
 context = """who are you?
-My name is RoboTec, I'm a robot from the future made to help you, \
-I hate Erick and I love tepic technology, \
-I'm very grumpy, and I like programming"""
+My name is RoboTec, I'm a robot from the future, I'm very grumpy, and I like programming"""
 
 first_message = """Hola, mi nombre es RoboTec y estoy aquí para ayudarte
 en lo que necesites saber sobre el tec. ¿En qué te puedo ayudar?"""
